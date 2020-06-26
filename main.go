1c20117d049a5c685a68b526f2c692643578ec6c
Hasi6 <hasitha.chandula@gmail.com>
2020-06-26 08:44:14 +0530

    create get requests using gorilla/mux

--- main.go
+++ main.go
@@ -1,10 +1,80 @@
 package main
 
 import (
-	"fmt"
-	_ "fmt"
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
 )
 
+
+//Inti books var as a slice Book Struct
+var books []Book
+
+
+
+// Book Struct (Model)
+type Book struct {
+	ID string `json:"id"`
+	Isbn string `json:"isbn"`
+	Title string `json:"title"`
+	Author *Author `json:"author"`
+}
+
+type Author struct {
+	FirstName string `json:"firstName"`
+	LastName string `json:"lastName"`
+}
+
+//Get All Books
+func getBooks(w http.ResponseWriter, r *http.Request)  {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(books)
+}
+
+// Get Single Book
+func getBook(w http.ResponseWriter, r *http.Request)  {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r) // Get All Params
+	// Loop throught books and find Id
+	for _, item := range books {
+		if item.ID == params["id"]{
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Book{})
+}
+
+// Create a New Book
+func createBook(w http.ResponseWriter, r *http.Request)  {
+
+}
+
+// Update Book
+func updateBook(w http.ResponseWriter, r *http.Request)  {
+
+}
+// Delete Book
+func deleteBook(w http.ResponseWriter, r *http.Request)  {
+
+}
 func main()  {
-	fmt.Println("Hello World")
+	// Init Router
+	r := mux.NewRouter()
+
+	// Mock Data
+	books = append(books, Book{ID: "1", Isbn: "676767", Title: "Book One", Author: &Author{FirstName: "Hasi", LastName: "Chandu"}})
+
+
+	// Route Handlers
+	r.HandleFunc("/api/books", getBooks).Methods("GET")
+	r.HandleFunc("/api/books/{id}", getBook).Methods("GET")
+	r.HandleFunc("/api/books", createBook).Methods("POST")
+	r.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
+	r.HandleFunc("/api/books/{id}",deleteBook).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":5000", r))
+
+
 }
